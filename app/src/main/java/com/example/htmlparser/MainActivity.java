6a4bedb92f6097969d35b890f8f073d665d862fe
undencem <undencem@gmail.com>
2021-02-07 03:54:35 +0300

    Initial commit

--- app/src/main/java/com/example/htmlparser/MainActivity.java
+++ app/src/main/java/com/example/htmlparser/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.htmlparser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String html = "<p>An <a href='http://example.com'><b>example</b></a>link</p>";
+
+        TextView textView = (TextView)findViewById(R.id.tv);
+        Document doc = Jsoup.parse(html);
+        Element link = doc.select("a").first();
+        String text = doc.body().text();
+        String linkHref=link.attr("href");
+        String linkText=link.text();
+        String linkOuterH = link.outerHtml();
+        String linkInnerH = link.html();
+
+        String result = text + "\n"+linkText+"\n"+linkOuterH+"\n"+linkInnerH;
+
+        textView.setText(result);
     }
 }
